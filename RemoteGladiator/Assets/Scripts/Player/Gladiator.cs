6343637e1c0ec66676f2359e6f8ee2b4dd746693
Roberto <roberto.coviello@bearingpoint.com>
2022-11-05 14:43:20 +0100

    Replace Mesh Colliders

--- RemoteGladiator/Assets/Scripts/Player/Gladiator.cs
+++ RemoteGladiator/Assets/Scripts/Player/Gladiator.cs
@@ -5,7 +5,8 @@ namespace Player
     public class Gladiator : MonoBehaviour
     {
         [SerializeField] private Transform playerTransform;
-        [SerializeField] private float moveSpeed;
+        [SerializeField] private float moveSpeed = 5;
+        [SerializeField] private float rotationSpeed = 5;
 
         private bool _buttonPressed;
         private Vector2 _joystickValue;
@@ -15,11 +16,17 @@ namespace Player
             //if (!_buttonPressed) return;
 
             // Get current player position
-            var playerPosition = playerTransform.localPosition;
+            var playerPosition = transform.localPosition;
 
             // Calculate player velocity and new position
             playerPosition += new Vector3(_joystickValue.x * moveSpeed * Time.deltaTime, 0f,
                 _joystickValue.y * moveSpeed * Time.deltaTime);
+
+            // Look in direction of movement
+            Vector3 moveDirection = new Vector3(_joystickValue.x, 0f, _joystickValue.y);
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            playerTransform.rotation =
+                Quaternion.RotateTowards(playerTransform.rotation, toRotation, rotationSpeed * Time.deltaTime);
 
             // Update player position
             playerTransform.localPosition = playerPosition;
