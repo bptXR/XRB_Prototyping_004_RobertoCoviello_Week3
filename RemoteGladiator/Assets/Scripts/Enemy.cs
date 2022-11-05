6b1e0a864213c794cb83c0dc41262aa44e7e0116
Roberto <roberto.coviello@bearingpoint.com>
2022-11-05 13:09:51 +0100

    Environment

---
+++ RemoteGladiator/Assets/Scripts/Enemy.cs
@@ -0,0 +1,153 @@
+using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+using Random = UnityEngine.Random;
+
+namespace Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private int maxHealth = 100;
+        [SerializeField] private int currentHealth;
+        //[SerializeField] private HealthBar healthBar;
+        [SerializeField] private SkinnedMeshRenderer meshRenderer;
+        [SerializeField] private int damageToPlayer = 20;
+
+        public int cost;
+        public Enemy enemyPrefab;
+
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip[] attackSounds;
+        [SerializeField] private AudioClip[] gettingHitSounds;
+        [SerializeField] private AudioClip[] dieSounds;
+        [SerializeField] private AudioClip walkingSound;
+
+        //private Player _player;
+        private NavMeshAgent _enemy;
+        [SerializeField] private Transform _playerTransform;
+        private Animator _anim;
+
+        private bool _isDead;
+        private bool _isAttacking;
+        private bool _isWalking = true;
+        private int _damageToEnemy;
+
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        private static readonly int AttackIndex = Animator.StringToHash("AttackIndex");
+        private static readonly int Die = Animator.StringToHash("Die");
+        private static readonly int DieIndex = Animator.StringToHash("DieIndex");
+        private static readonly int Walk = Animator.StringToHash("Walk");
+
+        private void Awake()
+        {
+            _enemy = GetComponent<NavMeshAgent>();
+            // _player = FindObjectOfType<Player>();
+            // _playerTransform = _player.transform;
+            _anim = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            currentHealth = maxHealth;
+            //healthBar.SetMaxHealth(maxHealth);
+            _enemy.speed = Random.Range(1.5f, 2.8f);
+        }
+
+        private void Update()
+        {
+            if (!_isDead)
+                _enemy.SetDestination(_playerTransform.position);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player") || _isDead) return;
+            _enemy.isStopped = true;
+            _isWalking = false;
+            _isAttacking = true;
+            _anim.SetInteger(AttackIndex, Random.Range(0, 7));
+            _anim.SetTrigger(Attack);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag("Player") || _isDead) return;
+            _enemy.isStopped = false;
+            _isWalking = true;
+            _isAttacking = false;
+            _anim.SetTrigger(Walk);
+        }
+
+        // public void Hit(Arrow arrow)
+        // {
+        //     DisableCollider(arrow);
+        //
+        //     if (!_isDead)
+        //         TakeDamage(arrow);
+        // }
+        //
+        // private void DisableCollider(Arrow arrow)
+        // {
+        //     if (arrow.TryGetComponent(out Collider collider))
+        //         collider.enabled = false;
+        // }
+        //
+        // private void TakeDamage(Arrow arrow)
+        // {
+        //     _damageToEnemy = arrow.damageToEnemy;
+        //     currentHealth -= _damageToEnemy;
+        //     healthBar.SetHealth(currentHealth);
+        //
+        //     if (currentHealth <= 0)
+        //     {
+        //         KillEnemy();
+        //     }
+        //     else
+        //     {
+        //         Sounds(gettingHitSounds);
+        //     }
+        // }
+
+        private void KillEnemy()
+        {
+            _isDead = true;
+            _enemy.isStopped = true;
+            _anim.SetInteger(DieIndex, Random.Range(0, 7));
+            _anim.SetTrigger(Die);
+
+            Sounds(dieSounds);
+
+            meshRenderer.materials[0].DOFade(0, 5).OnComplete(() => Destroy(gameObject));
+            // Destroy(this.GetComponent<Rigidbody>());
+            Destroy(_enemy);
+            //   Destroy(this.GetComponent<c>());
+        }
+
+        // private void OnDestroy()
+        // {
+        //     if (GameObject.FindGameObjectWithTag("WaveSpawner") == null) return;
+        //     GameObject.FindGameObjectWithTag("WaveSpawner").GetComponent<WaveSpawner>().spawnedEnemies
+        //         .Remove(enemyPrefab);
+        // }
+
+        private void Sounds(AudioClip[] clips)
+        {
+            AudioClip clip = clips[Random.Range(0, clips.Length)];
+            audioSource.PlayOneShot(clip);
+        }
+
+        // public void DoDamage()
+        // {
+        //     Sounds(attackSounds);
+        //
+        //     if (!_isAttacking) return;
+        //     int currentPlayerHealth = _player.currentHealth - damageToPlayer;
+        //     _player.TakeDamage(currentPlayerHealth);
+        // }
+
+        public void FootStep()
+        {
+            audioSource.PlayOneShot(walkingSound);
+        }
+    }
+}
--- RemoteGladiator/Assets/Scripts/Player/Gladiator.cs
+++ RemoteGladiator/Assets/Scripts/Player/Gladiator.cs
@@ -1,148 +1,33 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 
-namespace Script.Player
+namespace Player
 {
     public class Gladiator : MonoBehaviour
     {
-        [SerializeField]
-        [Tooltip("The claw transform that will be updated and translated")]
-        Transform m_ClawTransform;
+        [SerializeField] private Transform playerTransform;
+        [SerializeField] private float moveSpeed;
 
-        [SerializeField]
-        [Tooltip("The claw socket used to get the prizes")]
-        XRSocketInteractor m_ClawSocket;
+        private bool _buttonPressed;
+        private Vector2 _joystickValue;
 
-        [SerializeField]
-        [Tooltip("The claw speed when not carrying a prize")]
-        float m_ClawWithoutPrizeSpeed;
-
-        [SerializeField]
-        [Tooltip("The claw speed when carrying a prize")]
-        float m_ClawWithPrizeSpeed;
-
-        [SerializeField]
-        [Tooltip("The claw speed when the UfoAbductionForce is enabled")]
-        float m_ClawAbductionSpeed;
-
-        [SerializeField]
-        [Tooltip("The claw's minimum local position. Used to clamp the claw position")]
-        Vector2 m_MinClawPosition;
-
-        [SerializeField]
-        [Tooltip("The claw's maximum local position. Used to clamp the claw position")]
-        Vector2 m_MaxClawPosition;
-
-        [SerializeField]
-        [Tooltip("The Sparklies particle. This particle is activated while the UfoAbductionForce is enabled")]
-        ParticleSystem m_SparkliesParticle;
-
-        [SerializeField]
-        [Tooltip("The UfoBeam particle. This particle is activated while the PushButton is held down")]
-        ParticleSystem m_UfoBeamParticle;
-
-        bool m_ButtonPressed;
-        Vector2 m_JoystickValue;
-
-        void Start()
+        private void Update()
         {
-            StartCoroutine(NoPrizeState());
+            //if (!_buttonPressed) return;
+
+            // Get current player position
+            var playerPosition = playerTransform.localPosition;
+
+            // Calculate player velocity and new position
+            playerPosition += new Vector3(_joystickValue.x * moveSpeed * Time.deltaTime, 0f,
+                _joystickValue.y * moveSpeed * Time.deltaTime);
+
+            // Update player position
+            playerTransform.localPosition = playerPosition;
         }
 
-        void UpdateClawPosition(float speed)
-        {
-            // Get current claw position
-            var clawPosition = m_ClawTransform.localPosition;
-
-            // Calculate claw velocity and new position
-            clawPosition += new Vector3(m_JoystickValue.x * speed * Time.deltaTime, 0f,
-                m_JoystickValue.y * speed * Time.deltaTime);
-
-            // Clamp claw position
-            clawPosition.x = Mathf.Clamp(clawPosition.x, m_MinClawPosition.x, m_MaxClawPosition.x);
-            clawPosition.z = Mathf.Clamp(clawPosition.z, m_MinClawPosition.y, m_MaxClawPosition.y);
-
-            // Update claw position
-            m_ClawTransform.localPosition = clawPosition;
-        }
-
-        IEnumerator NoPrizeState()
-        {
-            // Move the claw
-            while (!m_ButtonPressed)
-            {
-                UpdateClawPosition(m_ClawWithoutPrizeSpeed);
-                yield return null;
-            }
-
-            StartCoroutine(TryGrabPrizeState());
-        }
-
-        IEnumerator TryGrabPrizeState()
-        {
-            // Start particles, activate the Socket and the UfoAbductionForce
-            m_SparkliesParticle.Play();
-            m_UfoBeamParticle.Play();
-            m_ClawSocket.socketActive = true;
-            //m_UfoAbductionForce.enabled = true;
-
-            // Try get a prize, the claw can still move
-            while (m_ButtonPressed && !m_ClawSocket.hasSelection)
-            {
-                UpdateClawPosition(m_ClawAbductionSpeed);
-                yield return null;
-            }
-
-            // Disable abduction force and the Sparklies particle
-            //m_UfoAbductionForce.enabled = false;
-            m_SparkliesParticle.Stop();
-
-            StartCoroutine(ReleasePrizeState());
-        }
-
-        IEnumerator ReleasePrizeState()
-        {
-            // Move the claw
-            while (m_ButtonPressed)
-            {
-                UpdateClawPosition(m_ClawWithPrizeSpeed);
-                yield return null;
-            }
-
-            // Release the prize and stop the last particle
-            m_ClawSocket.socketActive = false;
-            m_UfoBeamParticle.Stop();
-
-            StartCoroutine(NoPrizeState());
-        }
-
-        /// <summary>
-        /// Updates the internal state of the push button used by this class.
-        /// Called by the <c>XRPushButton.OnPress</c> event.
-        /// </summary>
-        public void OnButtonPress()
-        {
-            m_ButtonPressed = true;
-        }
-
-        /// <summary>
-        /// Updates the internal state of the push button used by this class.
-        /// Called by the <c>XRPushButton.OnRelease</c> event.
-        /// </summary>
-        public void OnButtonRelease()
-        {
-            m_ButtonPressed = false;
-        }
-        
-        public void OnJoystickValueChangeX(float x)
-        {
-            m_JoystickValue.x = x;
-        }
-        
-        public void OnJoystickValueChangeY(float y)
-        {
-            m_JoystickValue.y = y;
-        }
+        public void OnButtonPress() => _buttonPressed = true;
+        public void OnButtonRelease() => _buttonPressed = false;
+        public void OnJoystickValueChangeX(float x) => _joystickValue.x = x;
+        public void OnJoystickValueChangeY(float y) => _joystickValue.y = y;
     }
 }
